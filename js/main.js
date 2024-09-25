a16b82690113488929f44f1cb617e44d9b510711
rafaelramireza <111542208+rafaelramireza@users.noreply.github.com>
2024-09-25 16:53:05 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+﻿// Script para controlar el carrusel
+let currentSlide = 0;
+const slides = document.querySelectorAll('#carousel img');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Mostrar el primer slide al cargar la página
+showSlide(currentSlide);
